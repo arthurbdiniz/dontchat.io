f7b0e7ee58a77ed5ec74fff34a77c182fc126a97
Vitor Falcao <vitorfhcosta@gmail.com>
2018-01-21 13:58:30 -0200

    Enhancements on app js code

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
+// packages
 var express = require('express');
 var path = require('path');
-var mongoose = require('mongoose');
-var app = express();
 
+// nested routes
 var index = require('./routes/index');
 var chat = require('./routes/chat');
 
+// app setup
+var app = express();
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.set('port', process.env.PORT || 3000);
-
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routes setup
 app.use('/', index);
 app.use('/:chatCode', chat);
 
-module.exports = app;
+var server = app.listen(app.get('port'), function () {
+    console.log(`Listening on port ${app.get('port')}`);
+});
